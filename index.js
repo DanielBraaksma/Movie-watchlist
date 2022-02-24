1a4da89f686d61253927053426760caf600b68da
Daniel Braaksma <danebraaksma@hotmail.com>
2022-02-24 14:33:18 -0500

    basic layout and styling to pictures and movie info

--- index.js
+++ index.js
@@ -22,12 +22,16 @@ function renderSearch () {
     let html = ""
     searchResults.forEach(movie =>{
         console.log(movie);
-        html+= `<div><img src="${movie.Poster}">
-                    <p>${movie.Title}</p>
-                    <p>${movie.Year}</p>
-                    <p>${movie.Type}</p>
-                    <a href="https://www.imdb.com/title/${movie.imdbID}">IMDB</a>
-                    </div>`
+        html+= `<div class="movie-container">
+                    <img src="${movie.Poster}">
+                    <div class="movie-info">
+                        <p>Title: ${movie.Title}</p>
+                        <p>Year Released: ${movie.Year}</p>
+                        <p>Type: ${movie.Type}</p>
+                        <a href="https://www.imdb.com/title/${movie.imdbID}">IMDB</a>
+                        <button id="watchlist">Add to watchlist</button>
+                    </div>
+                </div>`
     })
     //add all the html to the searchContainer once
     searchContainer.innerHTML = html
