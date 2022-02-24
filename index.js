fa5313f18a0248c9ad23ba1a47d3935002e6c32a
Daniel Braaksma <danebraaksma@hotmail.com>
2022-02-24 13:57:48 -0500

    api calls and rendering on page

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-let search = document.getElementById("site-search")
+let searchBar = document.getElementById("site-search")
+let searchResults = []
+let searchContainer = document.getElementById("search-result-content")
 
 
 const searchBtn = document.getElementById("search-btn")
 
-// console.log(searchBtn)
 
 searchBtn.addEventListener("click", function(){
-    // console.log(search.value)
-    // fetch(`http://www.omdbapi.com/?apikey=ad56d582&s=${search.value}`)
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ad56d582&s=${search.value}`)
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ad56d582&s=${searchBar.value}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        // console.log(data)
+        searchResults = data.Search
+        console.log(searchResults)
+        renderSearch ();
     })
+
 })
+
+function renderSearch () {
+    let html = ""
+    searchResults.forEach(movie =>{
+        console.log(movie);
+        html+= `<div><img src="${movie.Poster}">
+                    <p>${movie.Title}</p>
+                    <p>${movie.Year}</p>
+                    <p>${movie.Type}</p>
+                    <a href="https://www.imdb.com/title/${movie.imdbID}">IMDB</a>
+                    </div>`
+    })
+    //add all the html to the searchContainer once
+    searchContainer.innerHTML = html
+}
