2b9df0d8d3e39469d6545ade11e79969a2b1e2d7
Daniel Braaksma <danebraaksma@hotmail.com>
2022-02-24 11:48:51 -0500

    api request working

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const searchBtn = document.getElementById("search-btn")
 searchBtn.addEventListener("click", function(){
     // console.log(search.value)
     // fetch(`http://www.omdbapi.com/?apikey=ad56d582&s=${search.value}`)
-    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=ad56d582&t=hello")
-        .then(res => res.JSON())
-        .them(data => console.log(data))
-
-} )
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ad56d582&s=${search.value}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+})
