f9891163945a8923299ef0cbfdcbcc4a0825f454
Daniel Braaksma <danebraaksma@hotmail.com>
2022-02-24 16:10:02 -0500

    version before trying out watchlist adding

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let searchBar = document.getElementById("site-search")
 let searchResults = []
 let searchContainer = document.getElementById("search-result-content")
+let myWatchlist = []
 
 
 const searchBtn = document.getElementById("search-btn")
@@ -20,19 +21,34 @@ searchBtn.addEventListener("click", function(){
 
 function renderSearch () {
     let html = ""
-    searchResults.forEach(movie =>{
-        console.log(movie);
+    // give each title and individual id
+    // increase the id number by one each time when creating it
+
+    // search through the searchResults, return index of the titleid.value
+        //push that the obj at that index to the watchlist.
+
+
+    searchResults.forEach((movie, i) =>{
+        // console.log(movie);
         html+= `<div class="movie-container">
                     <img src="${movie.Poster}">
                     <div class="movie-info">
                         <p>Title: ${movie.Title}</p>
                         <p>Year Released: ${movie.Year}</p>
                         <p>Type: ${movie.Type}</p>
-                        <a href="https://www.imdb.com/title/${movie.imdbID}">IMDB</a>
+                        <a href="https://www.imdb.com/title/${movie.imdbID}" target="_blank">IMDB</a>
                         <button id="watchlist">Add to watchlist</button>
                     </div>
                 </div>`
+
+
     })
-    //add all the html to the searchContainer once
+
     searchContainer.innerHTML = html
+    document.getElementById("watchlist").addEventListener("click", function(){
+        let
+        // myWatchlist.push(searchResults[$movie.Title])
+
+    })
+    console.log(myWatchlist)
 }
